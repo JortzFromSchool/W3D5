1e41f6a0c4da92e5c3f1c3c4d6686f77760d2f0b
spark <spark11491@gmail.com>
2021-08-27 13:50:46 -0400

    finish parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(node)
+        parent = node
+        node.children << self if parent != nil && node.children.include?(self)
+        node.children.delete(self)
+    end
 end

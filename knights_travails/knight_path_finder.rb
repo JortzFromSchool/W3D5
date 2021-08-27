ac6a5fbab7c962256fdec35ab752587cf126c074
spark <spark11491@gmail.com>
2021-08-27 15:15:15 -0400

    finish new_move_position

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,6 +1,27 @@
 require_relative "poly_tree_node.rb"
 class KnightPathFinder
     def initialize(starting_position)
-    
+        @starting_position = starting_position
+        @considered_positions = [starting_position]
+    end
+
+    def self.valid_moves(pos)
+        possible_moves = [[1,2], [-1,2], [-1,-2], [1, -2], [2,1], [-2, 1], [-2, -1], [2, -1]]
+        moves = []
+        possible_moves.each do |move|
+            x = pos[0] + move[0]
+            y = pos[1] + move[1]
+            moves << [x, y] if x >= 0 && x < 8 && y >= 0 && y < 8
+        end
+        return moves
+    end
+
+    def new_move_positions(pos)
+        moves = self.valid_moves(pos)
+        moves.each do |move|
+            moves.delete(move) if @considered_positions.include?(move)
+        end
+        @considered_positions += moves
+        return moves
     end
 end

a5f1c72140e88d3133fc3adaf486eee630256d00
Jortz <johnnydortiz@gmail.com>
2021-08-30 10:36:12 -0400

    trace_path_back wip

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -29,17 +29,32 @@ class KnightPathFinder
     end
 
     def build_move_tree()
-        tree = PolyTreeNode.new(@starting_position)
-        arr = [tree]
+        root = PolyTreeNode.new(@starting_position)
+        arr = [root]
         until arr.empty?
             node = arr.shift
-
             new_move_positions(node.value).each do |move|
                 new_child = PolyTreeNode.new(move)
                 node.add_child(new_child)
             end
             arr += node.children
         end
-        tree
+        root
+    end
+
+    def find_path(end_pos)
+        root = self.build_move_tree
+        end_node = root.dfs(end_pos)
+        end_node
+    end
+
+    def trace_path_back(node)
+        nodes = []
+        curr_pos = node.value
+        until curr_pos == nil
+            nodes.unshift(curr_pos)
+            curr_pos = node.parent.value
+        end
+        return nodes
     end
 end
